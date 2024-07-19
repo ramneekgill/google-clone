97a15de8627b3cfd4a27179b04dbfcb5ce63a152
Ramneek Gill <ramneekg25@gmail.com>
2024-07-19 11:44:18 -0600

    initial setup components and file structure

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+            <div className='home__header'>
+                <div className='home__headerLeft'></div>
+                <div className='home__headerRight'></div>
+            </div>
+            <div className='home__body'></div>
+        </div>
+        
+    )
+}
+
+export default Home

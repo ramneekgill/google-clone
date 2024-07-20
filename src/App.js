11db04862745652b7a4b5625064bb7c894ebe5a5
Ramneek Gill <ramneekg25@gmail.com>
2024-07-20 13:47:43 -0600

    create and style header links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Home from './pages/Home.js'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-      <h1>Test</h1>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/search" element={<h1>SEARCH PAGE</h1>}>
+          </Route>
+          <Route path="/" element={<Home />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css';
 
 function Home() {
     return (
         <div className="home">
-            <h1>Home</h1>
             <div className='home__header'>
-                <div className='home__headerLeft'></div>
+                <div className='home__headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
                 <div className='home__headerRight'></div>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
             </div>
             <div className='home__body'></div>
         </div>
         
     )
 }
-
+    
 export default Home

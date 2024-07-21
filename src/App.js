220407df6066d874632ca481c4f0176f56af900b
Ramneek Gill <ramneekg25@gmail.com>
2024-07-21 12:30:58 -0600

    add appsicon to home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Home from './pages/Home.js'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/search" element={<h1>SEARCH PAGE</h1>}>
+          <Route path="/search" element={<h1>test search</h1>}>
           </Route>
           <Route path="/" element={<Home />}>
           </Route>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css';
+import AppsIcon from '@mui/icons-material/Apps';
+
 
 function Home() {
     return (
         <div className="home">
+             
             <div className='home__header'>
                 <div className='home__headerLeft'>
                     <Link to='/about'>About</Link>
@@ -13,6 +16,8 @@ function Home() {
                 <div className='home__headerRight'></div>
                     <Link to='/gmail'>Gmail</Link>
                     <Link to='/images'>Images</Link>
+                    <AppsIcon/>
+                   
             </div>
             <div className='home__body'></div>
         </div>

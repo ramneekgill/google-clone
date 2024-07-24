70b432a620b81207f544c948f9cb0e0c4f7acb92
Ramneek Gill <ramneekg25@gmail.com>
2024-07-24 11:34:39 -0600

    complete data layer and dispatch search term

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,12 +4,19 @@ import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
 function Search({ hideButtons = false }) {
+  const [{}, dispatch] = useStateValue();
   const [input, setInput] = useState("");
   const navigate = useNavigate();
   const search = (e) => {
     e.preventDefault();
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
     navigate("/search");
   };
   return (

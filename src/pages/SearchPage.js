1359076233565677ddcc978779e6fcd9f1aced4c
Ramneek Gill <ramneekg25@gmail.com>
2024-07-25 14:06:41 -0600

    fetch data from google api and save response

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SearchPage.css";
 import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
-
+  const { data } = useGoogleSearch(term);
+  console.log(data);
   return (
     <div className="searchPage">
       <div className="searchPage__header">
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import API_KEY from "./keys";
+const CONTEXT_KEY = "c017e588de0a4496c";
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+  return { data };
+};
+
+export default useGoogleSearch;
